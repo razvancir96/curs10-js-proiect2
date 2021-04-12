42802808c14e84b1b0657da2302c133f9173ec58
razvancir96 <40989335+razvancir96@users.noreply.github.com>
2021-04-12 23:26:26 +0300

    add project to GitHub

---
+++ header.js
@@ -0,0 +1,10 @@
+// Selectam div-ul gol.
+const root = document.querySelector('#root');
+
+// Adaugam HTML-ul din JS.
+// Daca vrem sa scriem un sir de caractere pe mai multe randuri, folosim backticks(``)
+root.innerHTML += `
+  <header>
+    <h1>Magazinul La Baiatu'</h1>
+  </header>
+`;
---
+++ navbar.js
@@ -0,0 +1,9 @@
+// Atentie! Pe root il avem deja, deoarece a fost incarcat de primul script.
+root.innerHTML += `
+  <nav>
+    <ul>
+      <li>Meniu</li>
+      <li>Despre noi</li>
+    </ul>
+  </nav>
+`;
