f90196f640a1ad7810266b587dab0471a9dc4bda
razvancir96 <40989335+razvancir96@users.noreply.github.com>
2021-04-24 22:49:10 +0300

    remove course 8 comments

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// index.js este primul fisier care se incarca, deci root va fi disponibil in toate celelalte fisiere.
 const root = document.querySelector('#root');
-// Vrem ca produsele sa fie disponibile in intreaga aplicatie, pentru a putea fi modificate.
 const products = [
     {
-        // Un telefon are nume si pret.
         name: 'Iphone XS',
         price: 4500
     },
@@ -18,9 +15,6 @@ const products = [
 ];
 
 function startRendering() {
-    // Randam(afisam) lista de produse, trimitand ca parametru produsele.
     renderProductList(products);
 }
-// DUPA ce s-au incarcat TOATE scripturile, apelam functia startRendering.
-// Facem asta pentru ca la momentul incarcarii lui index.js, nu stim cine e renderProductList.
 window.addEventListener('load', startRendering);
